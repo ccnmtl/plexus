9a8540e8106659c9f03bc6b054d743c5187a627c
Anders Pearson <anders@columbia.edu>
2012-09-10 11:33:22 -0400

    basic index view

--- plexus/main/views.py
+++ plexus/main/views.py
@@ -1,6 +1,7 @@
 from annoying.decorators import render_to
-
+from plexus.main.models import Server, Alias
 
 @render_to('main/index.html')
 def index(request):
-    return dict()
+    return dict(servers=Server.objects.all(),
+                aliases=Alias.objects.all())
