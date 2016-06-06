c1e2cf8898594faa93c693de245ee02fb8d2612c
Anders Pearson <anders@columbia.edu>
2016-06-06 15:55:56 +0100

    expose raw json of most recent grainlog

--- plexus/grainlog/urls.py
+++ plexus/grainlog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import GrainLogListView, GrainLogDetailView
+from .views import GrainLogListView, GrainLogDetailView, RawView
 
 urlpatterns = [
     url(r'^$', GrainLogListView.as_view(), name='grainlog-list'),
+    url(r'^raw/$', RawView.as_view(), name='grainlog-raw'),
     url(r'^(?P<pk>\d+)/$', GrainLogDetailView.as_view(),
         name='grainlog-detail'),
 ]
