d6c72f50ceadf08212fa2c23afad3705e752801a
Anders Pearson <anders@columbia.edu>
2016-11-18 17:18:31 +0000

    super basic search interface for project portfolio

---
+++ plexus/portfolio/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+from wagtail.wagtailsearch.models import Query
+
+from .models import Entry
+
+
+class Search(View):
+    def get(self, request):
+        # Search
+        search_query = request.GET.get('query', None)
+        if search_query:
+            search_results = Entry.objects.live().search(search_query)
+
+            # Log the query so Wagtail can suggest promoted results
+            Query.get(search_query).add_hit()
+        else:
+            search_results = Entry.objects.none()
+
+        # Render template
+        return render(request, 'portfolio/search_results.html', {
+            'search_query': search_query,
+            'search_results': search_results,
+        })
