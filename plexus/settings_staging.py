a1cc08215591e374bc8c52ef62df645c6e2af7df
Nikolas Nyby <nikolas@gnu.org>
2019-11-08 13:43:52 -0500

    migrate to sentry-sdk

--- plexus/settings_staging.py
+++ plexus/settings_staging.py
@@ -1,7 +1,9 @@
-# flake8: noqa
-from plexus.settings_shared import *
-from ccnmtlsettings.staging import common
 import os
+from django.conf import settings
+from plexus.settings_shared import *  # noqa: F403
+from ccnmtlsettings.staging import common
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 project = 'plexus'
 base = os.path.dirname(__file__)
@@ -10,12 +12,19 @@ locals().update(
     common(
         project=project,
         base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa: F405
         cloudfront="d1vy4q2u1y7bpg",
     ))
 
 try:
-    from plexus.local_settings import *
+    from plexus.local_settings import *  # noqa: F403
 except ImportError:
     pass
+
+if hasattr(settings, 'SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,  # noqa: F405
+        integrations=[DjangoIntegration()],
+        debug=True,
+    )
